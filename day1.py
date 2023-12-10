aab4f13792509674d544a7f450c351493deea393
Viivi Tuomi <viivi.tuomi@louhosdigital.fi>
2023-12-10 16:45:51 +0200

    Day 1 updated

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-with open('input1.txt') as f:
+with open('input-1.txt') as f:
     input = [ i.strip() for i in f.readlines() ]
 # input = [
 #     '1abc2',
@@ -6,21 +6,48 @@ with open('input1.txt') as f:
 #     'a1b2c3d4e5f',
 #     'treb7uchet',
 # ]
-
-#print(type('1'))
 #print(input)
 
 numbers = []
+def CheckNumbers(string):
+    numbers = []
+    writtenNumbers = [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+    ]
+
+    for i, word in enumerate(writtenNumbers): # Loop the written numbers
+        if word in string: # Check if there is written numbers 
+            digit = i + 1
+            numbers.append(digit)
+    # print('tulos:', numbers, string)
+    
+    for e in string: # Loop the letters of the input word
+        if e.isdigit(): # Check if is number
+            numbers.append(e)
+
+    # print('tulos:', numbers, string)
+    return numbers
+
+            
+# Loop the input    
 for string in input:
     digits = []
     
-    # Check if is number
+    CheckNumbers(string)
+
     for i, e in enumerate(string):
-        if e.isdigit():
+        if e.isdigit(): # Check if is number
             digits.append(e)
-    
-    # Combine first and last letter OR first and first
-    numbers.append(int(digits[0] + digits[-1]))
+            
+    numbers.append(int(digits[0] + digits[-1])) # Combine first and last letter OR first and first
 output = sum((numbers))
 
 print(output)
