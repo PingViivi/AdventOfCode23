a28e98b8cc90156f14bafab056e79bae2c0255c6
Viivi Tuomi <viivi.tuomi@louhosdigital.fi>
2023-12-10 15:52:25 +0200

    Day2 part2 done

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Repository for Advent of Code 2034. Tasks can be found here: https://adventofcode.com/2023
 
 * [Day 1](https://adventofcode.com/2023/day/1)
+* [Day 2](https://adventofcode.com/2023/day/2)
 
---
+++ day2.py
@@ -0,0 +1,57 @@
+with open('input-2.txt') as f:
+    input = [ i.strip() for i in f.readlines() ]
+
+# testinput = [
+#     'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+#     'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+#     'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+#     'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+#     'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green ',
+# ]
+
+class Game:
+    def __init__(self, data_str):
+        self.title, self.game_data_str = map(str.strip, data_str.split(':'))
+        self.games = self.parse_game_data()
+
+    def parse_game_data(self):
+        games = self.game_data_str.split(';')
+        parsed_games = []
+        for game_str in games:
+            items = game_str.split(',')
+            parsed_game = [item.strip() for item in items]
+            parsed_games.append(parsed_game)
+        return parsed_games
+
+    def __str__(self):
+        return f"{self.title}: {self.games}"
+
+game_objects = [Game(game_str) for game_str in input]
+
+# Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+def checkPossibleGames(game):
+    bag = [
+        '12 red',
+        '13 green',
+        '14 blue',
+    ]
+    ispossible = []
+    #print(game)
+    for rounds in game:
+        for string in rounds:
+            amount, color = string.split(' ')
+            #print(amount)
+            if ((color == 'red' and int(amount) > 12) or (color == 'green' and int(amount) > 13) or (color == 'blue' and int(amount) > 14)):
+                ispossible.append(False)
+                #possible.append(i)
+            else:
+                ispossible.append(True)
+    return all(ispossible)
+
+output = 0
+for i, game in enumerate(game_objects, start=1):
+    possible = checkPossibleGames(game.games)
+    if (possible):
+        output += i
+
+print(output)
